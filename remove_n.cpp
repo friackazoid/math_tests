58f4d65b1df7033d3f37133f1e37620b735ebfe5
Alina Stepina <alina.stepina@tra.ai>
2018-07-07 12:31:39 +0300

    Remove - erase idiom for vector

--- remove_n.cpp
+++ remove_n.cpp
@@ -2,24 +2,46 @@
 #include <iostream>
 #include <algorithm>
 
+#include <vector>
+#include <ctime>
+
 int main () {
+    const int N = std::rand() % 10 + 1;
+    std::cout << "Found and remove N=" << N << std::endl;
+
     std::list<int> l(10);
     std::srand(time(NULL));
-    std::generate(l.begin(), l.end(), [] () mutable { return std::rand() % 10 + 1; });
+    std::generate(l.begin(), l.end(), [](){ return std::rand() % 10 + 1; });
 
-    std::cout << "list: ";
-    for (int i: l) {
-        std::cout << i << " ";
-    }
-    std::cout << "\n";
-
-    l.remove(3);
-
-    std::cout << "list: ";
+    std::cout << "Random generated list: ";
     for (int i: l) {
         std::cout << i << " ";
     }
     std::cout << "\n";
 
 
+    l.remove(N);
+
+    std::cout << "List after std::list::remove: ";
+    for (int i: l) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+
+    std::vector<int> v(10);
+    std::srand(std::time(nullptr));
+    std::generate(v.begin(), v.end(), [](){return std::rand()%10 + 1;});
+
+    std::cout << "Random generated vector: ";
+    for (const auto &vi: v)
+        std::cout << vi << " ";
+    std::cout << std::endl;
+
+    v.erase(std::remove(v.begin(), v.end(), N), v.end());
+    
+    std::cout << "Vector after erase-remove idiom: ";
+    for (const auto &vi: v)
+        std::cout << vi << " ";
+    std::cout << std::endl;
 }
